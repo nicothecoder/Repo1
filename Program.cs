97269ce3a3951d69b058d417fd1300253753ec3d
BuildTools <unconfigured@null.spigotmc.org>
2020-12-05 14:45:19 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Repo1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What's your name?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Hello {input}!");
+            Console.Clear
+
+        }
+    }
+}
